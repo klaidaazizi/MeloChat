2abe016e07e0545356c640fdd47f876941303d0d
lauraculligan <culligan.l@northeastern.edu>
2022-07-14 19:17:08 -0400

    nav bar, login xml, welcome xml and activity, logo

---
+++ app/src/main/java/com/example/melochat/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.melochat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText textUsername, textPassword;
+    private Button buttonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/melochat/WelcomeActivity.java
@@ -0,0 +1,41 @@
+package com.example.melochat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private Button loginButton, signupButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        loginButton = findViewById(R.id.button_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signupButton = findViewById(R.id.button_signup);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomeActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+
+}
